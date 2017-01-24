da5f44ed9801ab1df43adcdff625e8302769a2b3
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-24 09:41:05 -0800

    second test: power-of

--- add-one.js
+++ add-one.js
@@ -1,5 +1,3 @@
 var num = parseInt(process.argv[2]) + 1 || 1;
-//console.log(num);
-
 
 process.stdout.write(`${num}` + '\n');
---
+++ test/power-of.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var child_process = require('child_process');
+
+describe('power-of app', function() {
+    it('returns first arg to the power of the second arg', function () {
+        var output = child_process.execFileSync(
+            'node',
+            ['power-of.js', '3', '3'],
+            { encoding: 'utf8' }
+        );
+        assert.equal(output, 27)
+    });
+})
