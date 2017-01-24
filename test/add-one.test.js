1d9ff46cc3332fba6d265d1a2352c818c233e399
sdcaulley <stephanie.caulley@gmail.com>
2017-01-24 09:12:20 -0800

    "tried to add a new line, but was not successful"

--- test/add-one.test.js
+++ test/add-one.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var run = require('./run.js');
 
 it('add 1 to first argument', function() {
-    assert.equal(run(['add-one.js', '6']), '7');
+    assert.equal(run(['add-one.js', '3']), '4');
 });
 
 it('test for NaN', function() {
