9d68b3789c7d4e5f6b62df3cc62deacfc15919f6
sdcaulley <stephanie.caulley@gmail.com>
2017-01-23 16:41:47 -0800

    "tested for Nan, green"

--- test/add-one.test.js
+++ test/add-one.test.js
@@ -4,3 +4,7 @@ var run = require('./run.js');
 it('add 1 to first argument', function() {
     assert.equal(run(['add-one.js', '6']), '7');
 });
+
+it('test for NaN', function() {
+    assert.equal(run(['add-one.js', 'foo']), 'NaN');
+})
