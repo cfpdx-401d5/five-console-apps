7a5832bda4288ed794a97521e61c781caca01042
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-24 10:51:40 -0800

    test four: day-greet

--- test/day-greet.test.js
+++ test/day-greet.test.js
@@ -5,7 +5,9 @@ describe('day-greet app', function() {
     it('adds 1 to argument', function() {
         var output = child_process.execFileSync(
             'node',
-            ['day-greet.js', ]
-        )
-    })
+            ['day-greet.js', 'brigitte', '14:58'],
+            { encoding: 'utf8' }
+        );
+        assert.equal(output, 'good afternoon brigitte');
+    });
 });
