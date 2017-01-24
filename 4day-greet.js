63b062c5fa3a73294f60d90973d4fc7e8b8c2a08
mericks <mericks@users.noreply.github.com>
2017-01-23 23:00:02 -0800

    dayGreet test | failing: dayGreet not defined

---
+++ 4day-greet.js
@@ -0,0 +1,12 @@
+function dayGreet(name, time) {
+    if (time <= 7) {
+        process.stdout.write(`good morning ${name}`);
+    } else if (time <= 15) {
+        process.stdout.write(`good afternoon ${name}`);
+    } else 
+        process.stdout.write(`good evening ${name}`);
+};
+
+
+
+
---
+++ test/4day-greet.test.js
@@ -0,0 +1,14 @@
+var child_process = require('child_process');
+var assert = require('assert');
+var fourDayGreet = require('../4day-greet.js');
+
+describe('test4', function() {
+    it('take 1st arg of name and 2nd arg of time to greet user', function() {
+        var name = 'john';
+        var time = 13;
+        assert.equal(dayGreet, 'good afternoon john');
+    })
+})
+
+
+
