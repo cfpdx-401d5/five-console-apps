064b2fd020101b04dc20ae6f34727f8ba188869e
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-24 10:37:33 -0800

    created files for day-greet

--- test/day-greet.test.js
+++ test/day-greet.test.js
@@ -1,16 +1,11 @@
-
 var assert = require('assert');
 var child_process = require('child_process');
 
-describe('add-one app', function() {
+describe('day-greet app', function() {
     it('adds 1 to argument', function() {
         var output = child_process.execFileSync(
             'node',
-            ['add-one.js', '3'],
-            { encoding: 'utf8' }
-        );
-        assert.equal(output, 4);
-    });
+            ['day-greet.js', ]
+        )
+    })
 });
-
-
