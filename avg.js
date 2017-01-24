6a69930f7c3b040f8607fbe079919a463a34dd58
mizutombo <thomas.shultz52@gmail.com>
2017-01-24 09:19:17 -0800

    commits first-pass at code and test for avg.js ... needs debug

---
+++ avg.js
@@ -0,0 +1,8 @@
+// returns the average of all supplied arguments
+var arguments = parseInt(process.argv[i]);
+var total = 0;
+for (var i = 2; i < arguments.length; i++) {
+  total += arguments[i];
+  total += 
+}
+var avg = total / arguments.length;
