38d4f36d5caf5b162e244e443294befdac331681
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-24 09:31:22 -0800

    first test: add-one

---
+++ add-one.js
@@ -0,0 +1,5 @@
+var num = parseInt(process.argv[2]) + 1 || 1;
+//console.log(num);
+
+
+process.stdout.write(`${num}` + '\n');
---
+++ test/add-one.test.js
@@ -0,0 +1,16 @@
+
+var assert = require('assert');
+var child_process = require('child_process');
+
+describe('add-one app', function() {
+    it('adds 1 to argument', function() {
+        var output = child_process.execFileSync(
+            'node',
+            ['add-one.js', "3"],
+            { endoding: 'utf8' }
+        );
+        assert.equal(output, 4);
+    });
+})
+
+
