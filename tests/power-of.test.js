24badfb9c2e561e42c56c2e5808a12a9db360289
meghanroserebecca <meghanroserebecca@gmail.com>
2017-01-24 09:17:07 -0800

    added unit tests for power of and day-greet

---
+++ tests/power-of.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var run = require('./run.js');
+
+describe('power-of app', function(){
+    it('returns one number raised to the power of another number', function(){
+        var output = run(['power-of.js', 2, 3]);
+        assert.deepEqual(output, 8);
+    });
+});
