3a2e81ee74bf08b7895c788af8c20099b13d2b87
meghanroserebecca <meghanroserebecca@gmail.com>
2017-01-24 11:01:14 -0800

    created avg app and modified it and test to function

--- test/avg.test.js
+++ test/avg.test.js
@@ -3,7 +3,7 @@ var run = require('./run.js');
 
 describe('returning an average app', function(){
     it('averages an array of numbers', function() {
-        var output = run(['avg.js', [3, 4, 6, 0, 3]]);
-        assert.deepEqual(output, 3.2);
+        var output = run(['avg.js', 3, 4, 6, 0, 3]);
+        assert.deepEqual(output, '3.2');
     });
 });
