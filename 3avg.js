6962d804aa72c373819e5554120c6085c1080c6a
mericks <mericks@users.noreply.github.com>
2017-01-24 22:44:55 -0800

    3avg works when run node

--- 3avg.js
+++ 3avg.js
@@ -1,13 +1,14 @@
 var args = [];
 function argsArr() {
     for (i = 2; i < process.argv.length; i++) {
-        args.push(i);
+        args.push(+process.argv[i]);
     };
 };
+argsArr();
 
 function avg(args) {
     return args.reduce(function(a,b) {
         return a + b;
     }, 0) / args.length;
 }
-process.stdout.write(avg(args) + '\n'.toString());
+process.stdout.write(avg(args).toString());
