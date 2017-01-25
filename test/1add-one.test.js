acbbdf9a6656b0db586e821b30e447cedabedcd7
mericks <mericks@users.noreply.github.com>
2017-01-24 22:48:01 -0800

    removed console log

--- test/1add-one.test.js
+++ test/1add-one.test.js
@@ -5,7 +5,6 @@ var run = require('./run.js');
 describe('add-one app', function() {
     it('adds 1 to the first argument', function() {
         var output = run(['1add-one.js', '3']);
-        console.log(output);
         assert.equal(output, '4')
     });
 })
