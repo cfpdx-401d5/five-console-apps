de8d6c2cf31535efe2e09ff7da195449bf60f538
mericks <mericks@users.noreply.github.com>
2017-01-25 10:20:43 -0800

    3avg app refactored using array methods

--- 3avg.js
+++ 3avg.js
@@ -1,14 +1,20 @@
-var args = [];
-function argsArr() {
-    for (i = 2; i < process.argv.length; i++) {
-        args.push(+process.argv[i]);
-    };
-};
-argsArr();
+// function avg(args) {
+//     var args = [];
+//     for (i = 2; i < process.argv.length; i++) {
+//         args.push(+process.argv[i]);
+//     };
+//     return args.reduce(function(a,b) {
+//         return a + b;
+//     }, 0) / args.length;
+// }
+// process.stdout.write(avg(args).toString());
 
-function avg(args) {
-    return args.reduce(function(a,b) {
-        return a + b;
+// refactored with J's guidance 
+function calcAvg() {
+    var args = process.argv.slice(2);
+    return args.reduce(function(a, b) {
+        return a + parseInt(b);
     }, 0) / args.length;
 }
-process.stdout.write(avg(args).toString());
+
+process.stdout.write(`${calcAvg()}`);
