f6bec4fb194baad7492d57deee444fd189dece1f
mericks <mericks@users.noreply.github.com>
2017-01-24 21:46:02 -0800

    rewrote 4day-greet test

--- test/4day-greet.test.js
+++ test/4day-greet.test.js
@@ -1,14 +1,33 @@
-var child_process = require('child_process');
-var assert = require('assert');
-var fourDayGreet = require('../4day-greet.js');
 
-describe('test4', function() {
-    it('take 1st arg of name and 2nd arg of time to greet user', function() {
-        var name = 'john';
-        var time = 13;
-        assert.equal(dayGreet, 'good afternoon john');
-    })
-})
+var assert = require('assert');
+var run = require('./run.js');
+
+describe('day-greet app', function() {
+    it('1st arg of name and 2nd arg of time to greet user - afternoon', function() {
+        var output = run(['4day-greet.js', 'marty', '14:58']);
+        assert.equal(output, 'good afternoon marty');
+    });
+
+    it('1st arg of name and 2nd arg of time to greet user - morning', function() {
+        var output = run(['4day-greet.js', 'marty', '8:58']);
+        assert.equal(output, 'good morning marty');
+    });
+
+    it('1st arg of name and 2nd arg of time to greet user - evening', function() {
+        var output = run(['4day-greet.js', 'marty', '17:01']);
+        assert.equal(output, 'good evening marty');
+    });
+});
+
+
+// > node day-greet.js marty 14:58
+// good afternoon marty
+
+// > node day-greet.js marty 8:58
+// good morning marty
+
+// > node day-greet.js marty 17:01
+// good evening marty
 
 
 
