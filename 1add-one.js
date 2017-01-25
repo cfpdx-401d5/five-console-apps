aab72838ad79432b4a26032335e781cca47da37d
mericks <mericks@users.noreply.github.com>
2017-01-24 20:17:05 -0800

    addOne function working when called via node

--- 1add-one.js
+++ 1add-one.js
@@ -1,6 +1,7 @@
-exports.addOne = function (x) {
-    return x + 1;
-};
+var x = parseInt(process.argv[2]);
 
+function addOne(x) {
+  return x + 1;
+}
 
-
+process.stdout.write(addOne(x) + '\n'.toString());
