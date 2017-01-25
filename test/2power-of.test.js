a55c89b454c4f7759eca26c1eccc463e2fd0181f
mericks <mericks@users.noreply.github.com>
2017-01-24 21:05:01 -0800

    rewrote 2power-of test

--- test/2power-of.test.js
+++ test/2power-of.test.js
@@ -1,11 +1,10 @@
-var child_process = require('child_process');
-var assert = require('assert');
-var powerOf = require('../2power-of.js');
 
-describe('test2', function() {
+var assert = require('assert');
+var run = require('./run.js');
+
+describe('power-of app', function() {
     it('returns first argument to the power of the second argument', function() {
-        var x = 3;
-        var y = 3;
-        assert.equal(powerOf, 27);
+        var output = run(['2power-of.js', '3', '3'])
+        assert.equal(output, '27');
     })
-})
+});
