a6559b4c8ae10c23443ad343fa1b30a1ccf967d3
mericks <mericks@users.noreply.github.com>
2017-01-25 12:10:44 -0800

    fizz buzz passing

--- 5fizz-buzz.js
+++ 5fizz-buzz.js
@@ -4,15 +4,15 @@
 var n = process.argv[2];
 
 function fizzBuzz(n) {
-   result = '';
+   result = [];
    for (i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            result += 'fizzbuzz\n';
+            result.push('fizzbuzz');
         } else if (i % 3 === 0) {
-            result += 'fizz\n';
+            result.push('fizz');
         } else if (i % 5 === 0) {
-            result += 'buzz\n';
-        } else result += `${i}\n`;
+            result.push('buzz');
+        } else result.push(`${i}`);
    }
    return result;
 }
