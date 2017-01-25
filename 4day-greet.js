4daf5cc4581894f7291696debe493475f3b2b9a6
mericks <mericks@users.noreply.github.com>
2017-01-25 10:20:58 -0800

    4day-greet passing

--- 4day-greet.js
+++ 4day-greet.js
@@ -1,15 +1,16 @@
 var name = process.argv[2];
-var time = +process.argv[3];
-var greeting = '';
+var time = +process.argv[3].slice(0, -3);
 
 function dayGreet(time) {
-    if (time <= 9) {
-        return greeting = 'morning';
+    var greeting;
+    if (time <= 10) {
+        greeting = 'morning';
     } else if (time <= 16) {
-        return greeting = 'afternoon';
-    } else 
-        return greeting = 'evening';
-};
-dayGreet();
+        greeting = 'afternoon';
+    } else {
+        greeting = 'evening';
+    }
+    return greeting
+}
 
-process.stdout.write(`good ${greeting} ${name}`);
+process.stdout.write(`good ${dayGreet(time)} ${name}`);
