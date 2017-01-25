8518d896fcbe7b029d9fa2fe43a8cada13a80b93
mizutombo <thomas.shultz52@gmail.com>
2017-01-24 17:11:05 -0800

    adds afternoon and evening greeting tests to day-greet.js

--- test/day-greet.test.js
+++ test/day-greet.test.js
@@ -2,8 +2,20 @@ var assert = require('assert');
 var run = require('./run.js');
 
 describe('day-greet', function() {
-  it('takes first argument of name and second argument of 24hr time and writes time-appropriate greeting', function() {
+
+  it('returns time-appropriate greeting of "good morning" plus user-name for time between 00:00 and 12:00', function() {
     var actual = run(['day-greet.js', 'tom', '03:00']);
     assert.equal(actual, 'good morning tom');
   });
+
+  it('returns time-appropriate greeting of "good afternoon" plus user-name for time between 12:00 and 17:00', function() {
+    var actual = run(['day-greet.js', 'tom', '14:30']);
+    assert.equal(actual, 'good afternoon tom');
+  });
+
+  it('returns time-appropriate greeting of "good evening" plus user-name for time between 17:00 and 24:00', function() {
+    var actual = run(['day-greet.js', 'tom', '14:30']);
+    assert.equal(actual, 'good afternoon tom');
+  });
+
 });
