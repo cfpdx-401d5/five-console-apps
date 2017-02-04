0070855dd36bcfc4cfaffb29528f1d8d2edf78fc
Clarefollett <chlaw101@gmail.com>
2017-02-04 13:08:11 -0800

    power of test passes and fails

--- power-of.js
+++ power-of.js
@@ -3,4 +3,4 @@ var y = process.argv[3];
 
 var powerOf = Math.pow(Number(x), Number(y));
 
-process.stdout.write(String(powerOf));
+process.stdout.write(`${powerOf}`);
--- test/power-of.test.js
+++ test/power-of.test.js
@@ -3,7 +3,7 @@ var run = require('./run.js');
 
 describe('power-of', function() {
     it('should take the second number and assign it as the exponent for the first number and return the value', function() {
-        var output = run(['power-of.js', 453, 4143]);
-        assert.equal(output, 'Infinity');
-    })
+        var output = run(['power-of.js', 2, 3]);
+        assert.equal(output, 8);
+    });
 })
