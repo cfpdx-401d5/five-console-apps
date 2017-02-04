f0eda6c5e70668476d4b0e5d09a8e76f71c41392
Clarefollett <chlaw101@gmail.com>
2017-02-04 13:02:50 -0800

    changed add-one test number variable so that it works with any input

--- add-one.js
+++ add-one.js
@@ -1,5 +1,4 @@
-var number = process.argv[3] || 'could not recognize. try typing a number';
-
+var number = process.argv[2];
 var addOne = Number(number) + (1);
 
 process.stdout.write(String(addOne));
--- test/add-one.test.js
+++ test/add-one.test.js
@@ -3,7 +3,7 @@ var run = require('./run.js')
 
 describe('add-one', function() {
     it('should take the first argument and add one to it and then return the value of that new number', function() {
-        var output = run(['add-one.js', 3]);
-        assert.notEqual(output, 4);
+        var output = run(['add-one.js', 2]);
+        assert.notEqual(output, 2);
     });
 });
